12d379d8fa250ff0ed1be59c832f46d4fbb7554a
MOHD KHAJA <mohdkhaja.moinuddin521@gmail.com>
2025-09-29 16:39:05 +0530

    deplyment settingupdate

--- backend/deployment_settings.py
+++ backend/deployment_settings.py
@@ -4,14 +4,6 @@ import dj_database_url
 from .settings import *
 from .settings import BASE_DIR 
 
-ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
-
-CSRF_TRUSTED_ORIGINS = ['https://'+os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
-
-DEBUG = False
-
-SECRET_KEY = os.environ.get('SECRET_KEY')
-
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "corsheaders.middleware.CorsMiddleware",      # 👈 put it here
@@ -23,6 +15,22 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+
+ALLOWED_HOSTS = [
+    os.environ.get('RENDER_EXTERNAL_HOSTNAME'),
+    "calorie-tracking-frontend.onrender.com",
+    ]
+
+CSRF_TRUSTED_ORIGINS = [
+    f"https://{os.environ.get('RENDER_EXTERNAL_HOSTNAME')}",
+    "https://calorie-tracking-frontend.onrender.com",
+]
+
+DEBUG = False
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 
 # Allow frontend (React) origin
