87456cde2dd401c478cac343cf88031d7f699c1b
MOHD KHAJA <mohdkhaja.moinuddin521@gmail.com>
2025-09-30 10:58:13 +0530

    create superuser

--- build.sh
+++ build.sh
@@ -6,8 +6,7 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate # Apply database migrations
 
-# if [[$CREATE_SUPERUSER]];
-# then
-#     python manage.py createsuperuser --noinput
-# fi
-
+if [[$CREATE_SUPERUSER]];
+then
+    python manage.py createsuperuser --no-input
+fi
