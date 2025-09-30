80efa986d72ca4338c9bbd975676e5c79216e94f
MOHD KHAJA <mohdkhaja.moinuddin521@gmail.com>
2025-09-30 13:12:30 +0530

    Final commit

--- backend/urls.py
+++ backend/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.http import HttpResponse
 from django.contrib import admin
 from django.urls import path, include
 
+def home(request):
+    return HttpResponse("Welcome to Calorie Tracker Backend")
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
